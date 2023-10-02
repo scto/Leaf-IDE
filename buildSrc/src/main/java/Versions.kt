c8f89240e5caafa1b3dfc1630657cc15140b4356
SuMuCheng <3578557729@qq.com>
2023-10-02 21:25:12 +0800

    Upgrade java versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
     const val VERSION_NAME = "1.0.0.0"
 
     // Project option versions
-    val COMPILING_JAVA_VERSION = JavaVersion.VERSION_11
-    const val JVM_TARGET_VERSION = "11"
+    val COMPILING_JAVA_VERSION = JavaVersion.VERSION_17
+    const val JVM_TARGET_VERSION = "17"
     const val KOTLIN_COMPILER_EXTENSION_VERSION = "1.5.4-dev-k1.9.20-Beta-692cbee7ddd"
 }
